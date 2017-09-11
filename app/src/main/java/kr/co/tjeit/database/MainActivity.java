0ea25b1269f00be6a9dae18e88d042bab9b63304
cho881020 <cho881020@gmail.com>
2017-09-11 15:19:27 +0900

    메인액티비티 DB 생성

--- app/src/main/java/kr/co/tjeit/database/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/database/MainActivity.java
@@ -1,8 +1,20 @@
 package kr.co.tjeit.database;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import kr.co.tjeit.database.util.StudentDBManager;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.Button insertBtn;
+    private android.widget.Button updateBtn;
+    private android.widget.Button deleteBtn;
+    private android.widget.Button queryBtn;
+    private android.widget.EditText contentEdt;
+
+    public StudentDBManager mDBManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +23,8 @@ public class MainActivity extends BaseActivity {
         bindViews();
         setupEvents();
         setValues();
+
+
     }
 
     @Override
@@ -20,11 +34,16 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        mDBManager = StudentDBManager.getInstance(mContext);
     }
 
     @Override
     public void bindViews() {
 
+        this.contentEdt = (EditText) findViewById(R.id.contentEdt);
+        this.queryBtn = (Button) findViewById(R.id.queryBtn);
+        this.deleteBtn = (Button) findViewById(R.id.deleteBtn);
+        this.updateBtn = (Button) findViewById(R.id.updateBtn);
+        this.insertBtn = (Button) findViewById(R.id.insertBtn);
     }
 }
--- app/src/main/java/kr/co/tjeit/database/util/StudentDBManager.java
+++ app/src/main/java/kr/co/tjeit/database/util/StudentDBManager.java
@@ -14,12 +14,19 @@ public class StudentDBManager {
     static final String DB_STUDENT = "student.db";
 //    학생 정보가 저장되는 표 Table (excel : Sheet)
     static final String TABLE_STUDENT = "Students";
+
+//    DB 구조가 변경될때, 이를 버젼화 하여 기록하면 앱을 업데이트 해도 큰 문제 발생 X
     static final int DB_VERSION = 1;
 
     Context mContext;
 
+//    데이터베이스의 관리자의 경우엔 동시에 여러개의 객체가 생존해서 좋을게 없음.
+//    데이터베이스 관리 시스템은 단 하나의 객체만 존재하도록 보장.
+//     => 이러한 기법을 "Singletone" 패턴이라고 함.
+//     => 프로그래밍의 디자인패턴. (코드 설계 디자인)
     private static StudentDBManager mDbManager = null;
     private SQLiteDatabase mDB = null;
+
 
     public static StudentDBManager getInstance(Context context) {
         if (mDbManager == null) {
@@ -29,11 +36,15 @@ public class StudentDBManager {
         return mDbManager;
     }
 
+//    StudentDBManager 생성자.
+
     private StudentDBManager(Context context) {
         mContext = context;
 
+//        mDB는 새로만들거나, 기존의데이터베이스 체계를 받아옴.
         mDB = mContext.openOrCreateDatabase(DB_STUDENT, Context.MODE_PRIVATE, null);
 
+//        DB내부에 저장되 실제 표 (테이블) 만드는 과정.
         mDB.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_STUDENT +
                     "(" +
                     "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
